a5336b830a97be3c0cce2e55cd3b8c5f9aa1f659
Vlad Chudin <chudintttks@gmail.com>
2026-05-10 13:51:37 +0200

    add router

---
+++ src/components/TabsPage/TabsPage.tsx
@@ -0,0 +1,39 @@
+import { Link, useParams } from 'react-router-dom';
+import { Tab } from '../../types/Tab';
+import classNames from 'classnames';
+
+type Props = {
+  tabs: Tab[];
+};
+
+const TabsPage = ({ tabs }: Props) => {
+  const { tabId } = useParams();
+
+  const selectedTab = tabs.find(tab => tab.id === tabId) ?? null;
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={classNames({ 'is-active': tab.id === tabId })}
+            >
+              <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab ? selectedTab.content : 'Please select a tab'}
+      </div>
+    </>
+  );
+};
+
+export default TabsPage;
